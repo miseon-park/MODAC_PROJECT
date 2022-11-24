15ced1a60276ee9707d0bf6d90c9bc5fcebe6de9
miseon-park <72717093+miseon-park@users.noreply.github.com>
2022-11-24 17:27:06 +0900

    페이징 처리
    왜 얘는 안 올라감

--- MODAC/src/main/java/com/modac/camp/controller/CheckFindController.java
+++ MODAC/src/main/java/com/modac/camp/controller/CheckFindController.java
@@ -35,7 +35,56 @@ public class CheckFindController extends HttpServlet {
 		
 		request.setCharacterEncoding("UTF-8");
 		
+		
+		// 페이징 처리 시작
+		int listCount; //현재 총 게시글 갯수
+		int currentPage; //현재 페이지(즉, 사용자가 요청한 페이지)
+		int pageLimit; //페이지 하단에 보여질 페이징바의최대갯수
+		int boardLimit; //페이지에 보여질 게시글의 최댁 갯수
+		int maxPage; //가장 마지막 페이지가 몇 번쩨 페이지인지 총 페이지 수
+		int startPage; //페이징 하단에 보여질 페이징바의 시작수
+		int endPage; ///페이지 하단에 보여질 페이징바의 끝수
+		
+		// * listCount : 총 게시글 갯수
+		listCount = new CampService().selectListCount();
+		
+		// * currentPage : 현재 페이지
 		currentPage = Integer.parseInt(request.getParameter("currentPage") == null ? "1" : request.getParameter("currentPage"));
+		
+		// * pageLimit : 페이지 하단에 보여질 페이징바의 페이지 최대갯수(페이지목록들 몇개 단위씩)
+		pageLimit = 5;
+		
+		// * boardLimit : 한 페이지에 보여질 게시글의 최대갯수(게시글 몇개 단위씩)
+		boardLimit = 8;
+		
+		//* maxPage : 가장 마지막 페이지가 몇번 페이지인지(총 페이지수)
+		maxPage = (int)Math.ceil((double)listCount / boardLimit);
+		
+		// * startPage : 페이지 하단에 보여질 페이징 바의 시작수
+		startPage = (currentPage -1) / pageLimit * pageLimit + 1;
+		
+		//* endPage : 페이지 하단에 보여질 페이징바의 끝수
+		endPage = startPage + pageLimit -1;
+		
+		
+		if(endPage > maxPage) {
+			endPage = maxPage;
+		}
+		
+		//페이지 정보드를 하나의 공간에 담아서 보내기
+		//페이지 정보를 담은 vo클래스를 활용
+		// => 사진게시판. 공지사항 게시판에서도 사용할 수 있기 때문에 common
+		
+		//1. 페이징바 만들때 필요한 객체
+		PageInfo pi = new PageInfo(listCount, currentPage, pageLimit, boardLimit, maxPage, startPage , endPage);
+				
+		//2. 현재 사용자가 요청한 페이지에 보여줄 페이징바 객체 저달
+		//request.setAttribute("pi", pi);
+		
+		// 페이징 처리 끝
+
+		
+		
 		// 지역 검색
 		String loc1 = request.getParameter("loc1");
 		String loc2 = request.getParameter("loc2");
